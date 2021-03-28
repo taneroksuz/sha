f7092cdd70137d20b7ce568fb7d9d40111b58ea8
Taner Öksüz <taner.oeksuez@outlook.com>
2021-03-28 20:42:07 +0200

    basic functions and arrays of constants added.

--- cpp/sha.h
+++ cpp/sha.h
@@ -11,11 +11,37 @@ class SHA
 {
   private:
 
-    int K;
+    template <class T> T ROTL(T x, int n);
+
+    template <class T> T ROTR(T x, int n);
+
+    template <class T> T SHR(T x, int n);
+
+    template <class T> T ADD(T x, T y);
+
+    template <class T> T Ch(T x, T y, T z);
+
+    template <class T> T Maj(T x, T y, T z);
+
+    template <class T> T Parity(T x, T y, T z);
+
+    template <class T> T f(T x, T y, T z, int t);
+
+    template <class T> T SIGMA(T x, int t, int k);
+
+    template <class T> T sigma(T x, int t, int k);
+
+    uint32_t K(int t, int k);
 
   public:
 
-    SHA(int K);
+    void SHA224(uint8_t *in, uint8_t *out);
+
+    void SHA256(uint8_t *in, uint8_t *out);
+
+    void SHA384(uint8_t *in, uint8_t *out);
+
+    void SHA512(uint8_t *in, uint8_t *out);
 
 };
 
