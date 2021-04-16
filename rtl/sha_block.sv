4ac9b4ca0f326975da51fde3ea8a94bf5796cf7e
Taner Öksüz <taner.oeksuez@outlook.com>
2021-04-16 18:35:40 +0200

    generating massage block implemented but not tested.

--- rtl/sha_block.sv
+++ rtl/sha_block.sv
@@ -8,31 +8,48 @@ module sha_block
   input logic [0:0] Enable,
   input logic [0:0] Function,
   output logic [(Nb-1):0] Data,
-  output logic [(Nw-1):0] Index,
+  output logic [(Nm-1):0] Index,
   output logic [0:0] Ready
 );
   timeunit 1ns;
   timeprecision 1ps;
 
+  localparam  Nt = Nw/8;
+  localparam  Nleft = 112*Nt;
+  localparam  Nright = $clog2(8*Nt);
+
   localparam IDLE = 2'h0;
   localparam INIT = 2'h1;
   localparam END  = 2'h2;
 
+  logic [7:0] data_block [0:(Nl-1)];
+  logic [7:0] word [0:(Nt-1)];
+
+  logic [(Nw-1) : 0] data [0:15];
+
   typedef struct packed{
-    logic [(Nw-1) : 0] index;
+    logic [31 : 0] index;
+    logic [9 : 0] rest;
+    logic [4 : 0] i;
     logic [(Nm-1) : 0] size;
-    logic [(Nb-1) : 0] data;
+    logic [(Nw-1) : 0] w;
+    logic [(Nm-1) : 0] n;
     logic [1 : 0] state;
     logic [0 : 0] ready;
   } reg_type;
 
   reg_type init_reg = '{
     index : 0,
+    rest : 0,
+    i : 0,
     size : 0,
-    data : 0,
+    w : 0,
+    n : 0,
     state : IDLE,
     ready : 0
   };
+
+  integer i,j;
 
   reg_type r,rin;
   reg_type v;
@@ -45,29 +62,74 @@ module sha_block
 
       if (Enable == 1) begin
         if (Function == 0) begin
+          data_block = Data_Block;
           v.index = 0;
+          v.size = 0;
+          v.n = 0;
+        end else begin
+          v.n = v.n + 1;
         end
         v.state = INIT;
       end
+
+      v.i = 0;
 
       v.ready = 0;
 
     end else if  (r.state == INIT) begin
 
-      v.state = END;
+      for (j=0; j<Nt ;j=j+1) begin
+        word[j] = 0;
+      end
+
+      for (j=0; j<Nt ;j=j+1) begin
+        if (v.index == Nl) begin
+          word[j] = 8'h0;
+          v.state = END;
+        end else begin
+          word[j] = data_block[v.index];
+          v.size = v.size + 8;
+        end
+        v.index = v.index + 1;
+      end
+
+      for (j=0; j<Nt; j=j+1) begin
+        v.w[j*8 +: 8] = word[j];
+      end
+
+      data[v.i] = v.w;
+
+      v.i = v.i + 1;
 
       v.ready = 0;
 
     end else if  (r.state == END) begin
 
-      v.state = INIT;
+      v.rest = Nleft - (v.i << Nright);
 
-      v.ready = 0;
+      if (v.rest > 0) begin
+        v.w = 0;
+        v.ready = 0;
+      end else if (rest == 0) begin
+        v.w = v.size[(Nm-1):(Nm/2)];
+        v.ready = 0;
+      end else begin
+        v.w = v.size[(Nm/2-1):0];
+        v.state = INIT;
+        v.ready = 1;
+      end
+
+      data[v.i] = v.w;
+
+      v.i = v.i + 1;
 
     end
 
-    Data = v.data;
-    Index = v.index;
+    for (i=0; j<15; i=i+1) begin
+      Data[i*Nw +: Nw] = data[i];
+    end
+
+    Index = v.n;
     Ready = v.ready;
 
     rin <= v;
